83551fffe78747596c83184ee2c2ffbe290a371c
fpesce27 <fpesce@frba.utn.edu.ar>
2024-04-11 17:47:24 -0300

    asd

---
+++ src/main/java/com/TP_TACS_2024C1_GRUPO_1/TP_TACS_2024C1_GRUPO_1/TpTacs2024C1Grupo1Application.java
@@ -0,0 +1,13 @@
+package com.TP_TACS_2024C1_GRUPO_1.TP_TACS_2024C1_GRUPO_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpTacs2024C1Grupo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpTacs2024C1Grupo1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/TP_TACS_2024C1_GRUPO_1/TP_TACS_2024C1_GRUPO_1/TpTacs2024C1Grupo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TP_TACS_2024C1_GRUPO_1.TP_TACS_2024C1_GRUPO_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpTacs2024C1Grupo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
