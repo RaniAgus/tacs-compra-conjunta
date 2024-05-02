debdf85daeff78a710c556d0043e8b7c714ef34c
Agustin Ranieri <aguseranieri@gmail.com>
2024-05-01 22:48:46 -0300

    Agrupo handlers por status code

--- backend/src/main/java/ar/edu/utn/frba/tacs/tp2024c1/grupo1/exception/CredencialesInvalidas.java
+++ backend/src/main/java/ar/edu/utn/frba/tacs/tp2024c1/grupo1/exception/CredencialesInvalidas.java
@@ -1,13 +1,7 @@
 package ar.edu.utn.frba.tacs.tp2024c1.grupo1.exception;
 
-import org.springframework.http.HttpStatus;
-
 public class CredencialesInvalidas extends RuntimeException {
     public CredencialesInvalidas() {
         super("Credenciales inválidas");
-    }
-
-    public HttpStatus getStatusCode() {
-        return HttpStatus.UNAUTHORIZED;
     }
 }
