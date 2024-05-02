f241a6ddef0482b12a61a9b35269656de996034f
Agustin Ranieri <aguseranieri@gmail.com>
2024-05-01 23:27:34 -0300

    validacion de longitud de passwords

--- backend/src/main/java/ar/edu/utn/frba/tacs/tp2024c1/grupo1/dto/RegistrarseDTO.java
+++ backend/src/main/java/ar/edu/utn/frba/tacs/tp2024c1/grupo1/dto/RegistrarseDTO.java
@@ -2,7 +2,9 @@ package ar.edu.utn.frba.tacs.tp2024c1.grupo1.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
+import org.hibernate.validator.constraints.Length;
 
 @Builder
 public record RegistrarseDTO(
@@ -13,6 +15,7 @@ public record RegistrarseDTO(
     @Email
     String email,
 
-    @NotBlank(message = "La contraseña no puede estar vacía")
+    @NotNull(message = "La contraseña no puede estar vacía")
+    @Length(min = 8, message = "La contraseña debe tener al menos 8 caracteres")
     String contrasenia
 ) {}
