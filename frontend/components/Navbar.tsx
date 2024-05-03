b10c386f70aa4dc2acb8bdb9d70b16acf9b0ed06
Martin Lingeri <mlingeri@frba.utn.edu.ar>
2024-05-02 21:04:28 -0300

    cambios de estilo en navbar

--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -1,98 +1,143 @@
 "use client"
-import React from "react";
-import { Navbar as Nav, NavbarBrand, NavbarMenuToggle, NavbarMenuItem, NavbarMenu, NavbarContent, NavbarItem, Link, Button, Dropdown, DropdownTrigger, Avatar, DropdownMenu, DropdownItem, Input, Kbd } from "@nextui-org/react";
-import Session from "./Session";
-import { ThemeSwitcher } from "./ThemeSwitcher";
+import React, { useState } from "react"
+import {
+  Navbar as Nav,
+  NavbarBrand,
+  NavbarMenuToggle,
+  NavbarMenuItem,
+  NavbarMenu,
+  NavbarContent,
+  NavbarItem,
+  Link,
+  Button,
+  Dropdown,
+  DropdownTrigger,
+  Avatar,
+  DropdownMenu,
+  DropdownItem,
+  Input,
+  Kbd,
+} from "@nextui-org/react"
+import Session from "./Session"
+import { ThemeSwitcher } from "./ThemeSwitcher"
+import { usePathname } from "next/navigation"
 
 export default function Navbar() {
-    const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false)
+  const [loggedIn, setLoggedIn] = useState(false)
 
-    const menuItems = [
-        "Profile",
-    ];
+  const menuItems = [
+    { name: "Ver Articulos", page: "/articulos" },
+    { name: "Crear Publicacion", page: "/crear_publicacion" },
+    { name: "Mis Publicaciones", page: "/mis_publicaciones" },
+    { name: "Mis Compras", page: "/mis_compras" },
+  ]
 
-    const searchInput = (
-        <Input
-            aria-label="Search"
-            classNames={{
-                inputWrapper: "bg-default-100",
-                input: "text-sm",
-            }}
-            endContent={<Kbd className="hidden lg:inline-block" keys={["command"]}>K</Kbd>}
-            labelPlacement="outside"
-            placeholder="Search..."
-            type="search"
+  const pathname = usePathname()
+
+  return (
+    <Nav isBordered isMenuOpen={isMenuOpen} onMenuOpenChange={setIsMenuOpen}>
+      <NavbarContent className="sm:hidden" justify="start">
+        <NavbarBrand>
+          <Link href="/" className="font-bold text-inherit">
+            Compra Conjunta
+          </Link>
+        </NavbarBrand>
+      </NavbarContent>
+
+      <NavbarContent className="sm:hidden" justify="end">
+        <NavbarMenuToggle
+          aria-label={isMenuOpen ? "Close menu" : "Open menu"}
         />
-    );
+      </NavbarContent>
 
-    return (
-        <Nav
-            isBordered
-            isMenuOpen={isMenuOpen}
-            onMenuOpenChange={setIsMenuOpen}
-        >
-            <NavbarContent className="sm:hidden" justify="start">
-                <NavbarBrand>
-                    <p className="font-bold text-inherit">ACME</p>
-                </NavbarBrand>
-            </NavbarContent>
+      <NavbarContent className="hidden sm:flex gap-4" justify="center">
+        <NavbarBrand>
+          <Link href="/" className="font-bold text-inherit">
+            Compra Conjunta
+          </Link>
+        </NavbarBrand>
+        <NavbarItem isActive={pathname == "/articulos"}>
+          <Link color="foreground" href="/articulos">
+            Ver Articulos
+          </Link>
+        </NavbarItem>
 
-            <NavbarContent className="sm:hidden" justify="end">
-                <NavbarMenuToggle aria-label={isMenuOpen ? "Close menu" : "Open menu"} />
-            </NavbarContent>
+        {loggedIn && (
+          <>
+            <NavbarItem isActive={pathname == "/crear_publicacion"}>
+              <Link color="foreground" href="/crear_publicacion">
+                Crear Publicacion
+              </Link>
+            </NavbarItem>
+            <NavbarItem isActive={pathname == "/mis_publicaciones"}>
+              <Link color="foreground" href="/mis_publicaciones">
+                Mis Publicaciones
+              </Link>
+            </NavbarItem>
+            <NavbarItem isActive={pathname == "/mis_compras"}>
+              <Link color="foreground" href="/mis_compras">
+                Mis Compras
+              </Link>
+            </NavbarItem>
+          </>
+        )}
+      </NavbarContent>
 
-            <NavbarContent className="hidden sm:flex gap-4" justify="center">
-                <NavbarBrand>
-                    <p className="font-bold text-inherit">ACME</p>
-                </NavbarBrand>
-                <NavbarItem>
-                    <Link color="foreground" href="#">
-                        Features
-                    </Link>
-                </NavbarItem>
-                <NavbarItem isActive>
-                    <Link href="#" aria-current="page">
-                        Customers
-                    </Link>
-                </NavbarItem>
-                <NavbarItem>
-                    <Link color="foreground" href="#">
-                        Integrations
-                    </Link>
-                </NavbarItem>
-            </NavbarContent>
+      <NavbarContent className="hidden sm:flex" justify="end">
+        <ThemeSwitcher />
+        <Session />
+      </NavbarContent>
 
-            <NavbarContent className="hidden sm:flex" justify="end">
-                <ThemeSwitcher />
-                <NavbarItem className="hidden lg:flex">{searchInput}</NavbarItem>
-                <Session />
-            </NavbarContent>
-
-            <NavbarMenu>
-                {searchInput}
-                {menuItems.map((item, index) => (
-                    <NavbarMenuItem key={`${item}-${index}`}>
-                        <Link
-                            className="w-full"
-                            color="foreground"
-                            href="#"
-                            size="lg"
-                        >
-                            {item}
-                        </Link>
-                    </NavbarMenuItem>
-                ))}
-                <NavbarMenuItem key={"logout-2"}>
-                    <Link
-                        className="w-full"
-                        color="danger"
-                        href="#"
-                        size="lg"
-                    >
-                        Logout
-                    </Link>
-                </NavbarMenuItem>
-            </NavbarMenu>
-        </Nav>
-    );
+      <NavbarMenu>
+        {menuItems.map((item, index) => (
+          <NavbarMenuItem key={`${item}-${index}`}>
+            <Link
+              className="w-full"
+              color="foreground"
+              href={item.page}
+              size="lg"
+            >
+              {item.name}
+            </Link>
+          </NavbarMenuItem>
+        ))}
+        {loggedIn ? (
+          <NavbarMenuItem key={"logout-2"}>
+            <Link
+              className="w-full"
+              color="danger"
+              href="/cerrar_sesion"
+              size="lg"
+            >
+              Logout
+            </Link>
+          </NavbarMenuItem>
+        ) : (
+          <>
+            <NavbarMenuItem key={"login-2"}>
+              <Link
+                className="w-full"
+                color="primary"
+                href="/iniciar_sesion"
+                size="lg"
+              >
+                Iniciar Sesion
+              </Link>
+            </NavbarMenuItem>
+            <NavbarMenuItem key={"login-2"}>
+              <Link
+                className="w-full"
+                color="warning"
+                href="/registrarse"
+                size="lg"
+              >
+                Registrase
+              </Link>
+            </NavbarMenuItem>
+          </>
+        )}
+      </NavbarMenu>
+    </Nav>
+  )
 }
