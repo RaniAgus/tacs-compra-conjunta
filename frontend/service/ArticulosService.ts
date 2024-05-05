56b059e667dc13f243b57bf9ee2d105d09bda99a
fpesce27 <fpesce@frba.utn.edu.ar>
2024-05-05 14:46:59 -0300

    fixeo unos temitas con los parametros de crear_publicacion

--- frontend/service/ArticulosService.ts
+++ frontend/service/ArticulosService.ts
@@ -1,11 +1,10 @@
 "use server"
-
 import { Request } from "./AbstractService"
-import { CrearArticuloDTO } from "@/model/CrearArticuloDTO"
 
 
 export async function getArticulos() {
     return await Request("/articulos", "GET", {}, false)
+}
 
 export async function crearArticulo(articulo: CrearArticuloDTO) {
     return await Request("/articulos", "POST", articulo)
